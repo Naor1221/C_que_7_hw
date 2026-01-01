cb05476a53aaf07dd81f9b5e5cb430dc3c908f92
Naor Balas <naor56564@walla.com>
2026-01-01 05:16:59 +0200

    header

---
+++ que_7.h
@@ -0,0 +1,5 @@
+#ifndef DELETE_ce44579d215f450382e3bc0b959f8abe
+#define DELETE_ce44579d215f450382e3bc0b959f8abe
+#include <stdio.h>
+void arr_remove_at(int arr[], size_t *p_len, size_t index);
+#endif
